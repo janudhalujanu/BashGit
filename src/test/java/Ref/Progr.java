179a8dca141262d8ca700405988270a4d3897b57
janudhalujanu <nagarajujyothi98@gmail.com>
2023-05-15 17:35:32 +0530

    ok

---
+++ src/test/java/Ref/Progr.java
@@ -0,0 +1,11 @@
+package Ref;
+
+import org.testng.annotations.Test;
+
+public class Progr {
+	@Test 
+	public void Sim() {
+		System.out.print("bye");
+	}
+
+}
---
+++ src/test/java/Ref/Sigma.java
@@ -0,0 +1,9 @@
+package Ref;
+
+public class Sigma {
+
+	public  void Test() {
+System.out.print("papa");
+	}
+
+}
